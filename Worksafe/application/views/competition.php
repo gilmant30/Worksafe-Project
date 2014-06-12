4b7aec7facb421cda785c4aeb2268654bc2efca2
Tim Gilman <timothy.gilman@yahoo.com>
2014-06-12 10:00:15 -0500

    Add initial codeigniter framework
    admin login page view done

---
+++ Worksafe/application/views/competition.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link href="header.css" rel="stylesheet">
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+
+<script>
+
+</script>
+
+<title>Competition</title>
+</head>
+<body>
+<h1>Competition Front Page</h1>
+
+<div class="competition" id="links">
+<a href="http://babbage.cs.missouri.edu/~tmgy87/Worksafe/admin/new_competition.php">Start New Competition</a>
+
+<a href="#">Edit Competition</a>
+
+<a href="#">Delete Competition</a>
+
+<br /><br /><br />
+
+<a href="http://babbage.cs.missouri.edu/~tmgy87/Worksafe/admin/create_question.php">Create Questions</a>
+</div>
+
+</body>
+</html>
